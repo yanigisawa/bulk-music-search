4e07f6a4331dcd030211f6b16d2d2d061a970520
James Alexander <james@jamesralexander.com>
2013-07-04 12:00:38 -0400

    - Adding HTML form front-end for running bulk search via a web page

--- bulkSearch.py
+++ bulkSearch.py
@@ -3,8 +3,10 @@
 import json
 import urllib2
 import urllib
+import cgi;
+import cgitb; cgitb.enable()
 
-fileName = "searchTerms2.txt"
+fileName = "tmp.txt"
 
 def getYouTubeLinkFromJson(youTubeSearchJson):
     data = json.load(youTubeSearchJson)
@@ -26,6 +28,14 @@ def getSearchTermListFromFile():
 
     return searchTerms
 
+def getSearchTermListFromInput(formInput):
+    searchTerms = []
+    lines = [s.strip() for s in formInput.splitlines()]
+    for line in lines:
+        searchTerms.append(line)
+
+    return searchTerms
+
 def printHtmlFile(termTuple):
     beginHtml = "<html><body>"
     itemHtml = """
@@ -37,22 +47,27 @@ def printHtmlFile(termTuple):
     with open("index.html", 'w') as f:
         f.write(beginHtml)
         for item in termTuple:
-            f.write(itemHtml.format(urllib.unquote(item[0]), item[1]))
+            f.write(itemHtml.format(item[0], item[1]))
         f.write(endhtml)
 
 def printAjaxHtml(termTuple):
-    print("Content-Type: text/html")
-    print # blank line, end of header
+    #print("Content-Type: text/html")
+    #print # blank line, end of header
     itemHtml = """
         <h2>{0}</h2>
         <iframe width="420" height="315" src="{1}" frameborder="0" allowfullscreen></iframe>
     """
     for item in termTuple:
-        print(itemHtml.format(urllib.unquote(item[0]), item[1]))
+        print(itemHtml.format(item[0], item[1]))
     
 
 def main():
-    searchTerms = getSearchTermListFromFile()
+    form = cgi.FieldStorage()
+    searchInput = form.getvalue("searchTerms")
+    if searchInput:
+        searchTerms = getSearchTermListFromInput(searchInput)
+    else:
+        searchTerms = getSearchTermListFromFile()
 
     baseUrl = "https://gdata.youtube.com/feeds/api/videos?q={0}&v=2&alt=json&max-results=1"
     termTuple = []
